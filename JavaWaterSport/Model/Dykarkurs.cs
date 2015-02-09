109de533565571186d8e154392459382ba7b57fd
dethret <andreaspallin@live.se>
2015-02-09 10:53:49 +0100

    Skrivit kund class och Dykarkurs class

--- JavaWaterSport/Model/Dykarkurs.cs
+++ JavaWaterSport/Model/Dykarkurs.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace JavaWaterSport.Model
 {
+    [Serializable]
     class Dykarkurs
     {
+        private int Id;
+        private int time;
+        private string instruktör;
+        private string datum;
+
+        public Dykarkurs()
+        {
+            this.time = 0;
+            this.instruktör = "Ingen instruktör";
+            this.datum = "Inget datum";
+        }
     }
 }
--- JavaWaterSport/Model/Kund.cs
+++ JavaWaterSport/Model/Kund.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace JavaWaterSport.Model
 {
+    [Serializable]
     class Kund
     {
+        private int m_id;
+        private string m_personalId;
+        private string m_name;
+
+        public string Name
+        {
+            get {return m_name;}
+            set { m_name = value; }
+        }
+
+        public string PersonalID
+        {
+            get { return m_personalId; }
+            set { m_personalId = value; }
+        }
+
+        public int ID
+        {
+            get { return m_id; }
+            set { m_id = value; }
+        }
+
+        public Kund(string personalID, string name)
+        {
+            this.m_name = name;
+            this.m_personalId = personalID;
+        }
+
+        public override string ToString()
+        {
+            return m_personalId + " | " + m_name;
+        }
     }
 }
