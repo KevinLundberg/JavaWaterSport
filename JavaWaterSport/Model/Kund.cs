5ff42cdc6d84340b93359f993c305c5e2d1ce184
Mackan969 <marcus.ostmo@gmail.com>
2015-05-06 12:48:45 +0200

    Kommentarer

--- JavaWaterSport/Model/Kund.cs
+++ JavaWaterSport/Model/Kund.cs
@@ -14,30 +14,48 @@ namespace JavaWaterSportSystem
         private string personligId;
         private string namn;
 
+        /// <summary>
+        /// Set och return kundens namn
+        /// </summary>
         public string Namn
         {
             get {return namn;}
             set { namn = value; }
         }
 
+        /// <summary>
+        /// Set och return kundens personliga ID
+        /// </summary>
         public string PersonligID
         {
             get { return personligId; }
             set { personligId = value; }
         }
 
+        /// <summary>
+        /// Set och return ID
+        /// </summary>
         public int ID
         {
             get { return id; }
             set { id = value; }
         }
 
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="personligID">Kundens personliga ID</param>
+        /// <param name="name">Kundens namn</param>
         public Kund(string personligID, string name)
         {
             this.namn = name;
             this.personligId = personligID;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return personligId + " | " + namn;
